7238e58478efc98ebc2c4136b8210b42285b9ea9
Corbijn Bulsink <corbijn.bulsink@hotmail.com>
2023-05-30 10:54:45 -0700

    added decimal, percent, sign buttons

--- script.js
+++ script.js
@@ -4,15 +4,59 @@ var secondValue = null;
 
 var displayValue = null;
 
+var temp = false;
+
 const numbers = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
 
+const decimal = document.querySelector(".decimal");
+const sign = document.querySelector(".sign");
+const percent = document.querySelector(".percent");
+
+percent.addEventListener('click', event => {
+    if (displayValue !== null && temp === true) {
+        displayValue = (displayValue/100);
+        firstValue = displayValue;
+        updateDisplay();
+    } else if (displayValue !== null) {
+        displayValue = (displayValue/100);
+        updateDisplay();
+    }
+});
+
+sign.addEventListener('click', event => {
+    if (displayValue.includes("-")) {
+        displayValue = displayValue.replace('-', '');
+        updateDisplay();
+    } else {
+    displayValue = "-" + displayValue;
+    console.log(displayValue);
+    updateDisplay();
+    }
+});
+
+//adds decimal to displayValue string, only if there is not already a decimal present
+decimal.addEventListener('click', event => {
+    if (displayValue === null) {
+        displayValue = "0.";
+        updateDisplay();
+    } else if (displayValue.includes(".")) {
+    } else {
+        displayValue += ".";
+        updateDisplay();
+        console.log(displayValue);
+    }
+});
 
 let numbersArray = Array.from(numbers);
 numbersArray.forEach(button => {
     button.addEventListener('click', event => {
         if (displayValue == null) {
             displayValue = (event.target.id);
+        } else if (displayValue !== null & temp === true) {
+            clearAll();
+            displayValue = (event.target.id);
+            temp = false;
         } else {
             displayValue += (event.target.id);
         } 
@@ -23,7 +67,7 @@ numbersArray.forEach(button => {
 
 let operatorsArray = Array.from(operators);
 operatorsArray.forEach(button => {
-    button.addEventListener('click', event => {
+    button.addEventListener('click', event => { 
         if (displayValue !== null && firstValue === null) {
             firstValue = displayValue
             operator = (event.target.id);
@@ -35,7 +79,7 @@ operatorsArray.forEach(button => {
             console.log("secondValue: " + secondValue);
             console.log("--------------------------------------------------");
 
-        } else if (firstValue !== null) {
+        } else if (firstValue !== null & temp == false) {
             secondValue = displayValue;
             clearDisplay();
             firstValue = operate(Number(firstValue), operator, Number(secondValue));
@@ -43,13 +87,23 @@ operatorsArray.forEach(button => {
 
             console.log("--------------------------------------------------");
             console.log("pressed-operator " + operator);
-            console.log("firstvalue: " + firstValue);
+            console.log("firstValue: " + firstValue);
             console.log("secondValue: " + secondValue);
             console.log("--------------------------------------------------");
 
+        } else if (firstValue !== null & temp == true) {
+            clearDisplay();
+            operator = (event.target.id);
+            temp = false;
+            console.log("--------------------------------------------------");
+            console.log("pressed-operator " + operator);
+            console.log("firstValue: " + firstValue);
+            console.log("secondValue: " + secondValue);
+            console.log("--------------------------------------------------");
         }
     });
 });
+
 
 function updateDisplay() {
     display.textContent = displayValue;
@@ -66,17 +120,28 @@ function clearAll() {
     firstValue = null;
     secondValue = null;
     operator = null;
+    temp = false;
     console.log("clear All");
 }
 
 function equals() {
-    secondValue = displayValue;
+    if (temp === false) {
+        secondValue = displayValue;
+        displayValue = operate(Number(firstValue), operator, Number(secondValue));
+        updateDisplay();
+        firstValue = displayValue;
+        temp = true;  
+    }
+
+    console.log("--------------------------------------------------");
+    console.log("equals");
+    console.log("firstValue: " + firstValue);
     console.log("secondValue: " + secondValue);
-    displayValue = operate(Number(firstValue), operator, Number(secondValue));
-    updateDisplay();
-    console.log(operate(Number(firstValue), operator, Number(secondValue)));
+    console.log(temp)
+    console.log("--------------------------------------------------");
 }
 
+//
 function operate(a, op, b) {
     if (op === "+") {
         return add(a, b);
@@ -84,8 +149,8 @@ function operate(a, op, b) {
         return subtract(a, b);
     } else if (op === "*") {
         return multiply(a, b);
-    } else if (op === "-") {
-        return subtract(a, b);
+    } else if (op === "÷") {
+        return divide(a, b);
     }
 }
 
